bda542969247557d3d9a9205b5afc8e83be4cdb0
anusha <anushadevi.sunkara@gmail.com>
2018-04-26 17:37:20 +0530

    initial commit

---
+++ src/main/java/project6/pro6.java
@@ -0,0 +1,7 @@
+package project6;
+
+public class pro6 {
+	public String sample() {
+		return "Sample Demo Java Class";
+	}
+}
---
+++ src/test/java/project6/pro6.java
@@ -0,0 +1,13 @@
+package project6;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class pro6 {
+
+	@Test
+	public void test() {
+	}
+
+}
